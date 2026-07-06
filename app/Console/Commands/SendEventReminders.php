86647f2435f9666c7253c49521fa631eb7b3b099
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-07-06 13:06:26 +0200

    feat(SendEventReminders): make command queueable
    Implement ShouldQueue interface and use Queueable trait to
    allow the SendEventReminders command to be queued for
    asynchronous execution.

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -5,16 +5,20 @@ namespace App\Console\Commands;
 use App\Models\Attendee;
 use App\Models\Event;
 use App\Notifications\EventReminderNotification;
+use Illuminate\Bus\Queueable;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Str;
 
 /** @package App\Console\Commands */
 #[Signature('app:send-event-reminders')]
 #[Description("Send notifications to all event's attendees that event starts soon")]
-class SendEventReminders extends Command
+class SendEventReminders extends Command implements ShouldQueue
 {
+    use Queueable;
+
     /**
      * Execute the console command.
      */
